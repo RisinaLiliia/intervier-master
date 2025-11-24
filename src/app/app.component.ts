354ec62a92b43e5376746fce56145824741ba78f
Risina Liliia <risinalilia2@gmail.com>
2025-11-24 11:23:12 +0100

    add initial components

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { LeftSideMenuComponent } from './components/left-side-menu/left-side-menu.component';
+import { CategoryComponent } from './components/category/category.component';
+import { DeleteConfirmationModalComponent } from './components/delete-confirmation-modal/delete-confirmation-modal.component';
+import { GenerateAnswerModalComponent } from './components/generate-answer-modal/generate-answer-modal.component';
+import { PreparationComponent } from './components/preparation/preparation.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    CategoryComponent,
+    DeleteConfirmationModalComponent,
+    GenerateAnswerModalComponent,
+    LeftSideMenuComponent,
+    PreparationComponent,
+    TopMenuComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+    title = 'my-angular-app';
+}
---
+++ src/app/components/left-side-menu/left-side-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-side-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './left-side-menu.component.html',
+  styleUrl: './left-side-menu.component.scss'
+})
+export class LeftSideMenuComponent {
+
+}
